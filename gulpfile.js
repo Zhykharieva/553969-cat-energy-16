1497232bdc27ece6e1ae4dacbcb349a3d5d99441
Ata <zhykharieva@gmail.com>
2019-05-01 12:35:04 +0300

    photo

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,11 @@ var sass = require("gulp-sass");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var gulp = require("gulp");
+var rename = require("gulp-rename");
+var gulp = require('gulp');
+var webp = require('gulp-webp');
+var imagemin = require("gulp-imagemin");
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -35,3 +40,25 @@ gulp.task("server", function () {
 });
 
 gulp.task("start", gulp.series("css", "server"));
+
+gulp.task("webp", function () {
+return    gulp.src("source/img/**/*.{png, jpg}")
+.pipe(webp({quality: 90}))
+        .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("images", function () {
+return    gulp.src("source/img/**/*.{png, jpg, svg}")
+.pipe(imagemin([imagemin.optipng({optimizationLevel:3}),
+  imagemin.jpegtran({progressive: true}),
+  imagemin.svgo()
+]))
+        .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("sprite", function(){
+  return gulp.src("source/img/icon-*.svg")
+  .pipe(svgstore({ inlineSvg: true }))
+.pipe(rename("sprite.svg"))
+  .pipe(gulp.dest("source/img"));
+})
